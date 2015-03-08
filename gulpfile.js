5d0cbf733d8ae5d6f66624c958d7774c88d6d9a4
arvinsim <arthurvincent.simon@gmail.com>
2015-03-08 11:56:30 +0800

    Added initial gulpfile and images.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var jade = require("gulp-jade");
+
+gulp.task("default", function start() {
+    console.log('lol');
+});
+
+// TODO: Task for outputting scss to minified css
+// TODO: Task for outputting the template to a single, minified html file
+// TODO: Task for minifying images
