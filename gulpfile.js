49100b8572cbfe00b288aea3a840af680a1638d7
arvinsim <arthurvincent.simon@gmail.com>
2015-03-14 10:33:12 +0800

    Added task to compile jade index template.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var jade = require("gulp-jade");
+var plumber = require('gulp-plumber');
 
-gulp.task("default", function start() {
-    console.log('lol');
+gulp.task("default", ["jade"], function taskDefault() {
+    var watcher = gulp.watch("./index.jade", ["jade"]);
+});
+
+// TODO: Task for outputting the template to a single, minified html file
+gulp.task("jade", function taskJade(event) {
+    return gulp.src("./index.jade")
+        .pipe(jade({ pretty: true}))
+        .pipe(gulp.dest("./public/"));
 });
 
 // TODO: Task for outputting scss to minified css
-// TODO: Task for outputting the template to a single, minified html file
 // TODO: Task for minifying images
