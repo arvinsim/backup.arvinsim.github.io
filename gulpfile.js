32068e1e0c1da250a8cd8ab74b8dc36fe3843013
arvinsim <arthurvincent.simon@gmail.com>
2015-03-15 17:33:27 +0800

    Added Gulp configuration for compiling CSS and Javascript. Added main JS file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+//var sass = require("gulp-sass");
 var jade = require("gulp-jade");
-var plumber = require('gulp-plumber');
+var plumber = require("gulp-plumber");
 
-gulp.task("default", ["jade"], function taskDefault() {
-    var watcher = gulp.watch("./index.jade", ["jade"]);
+var directories = {
+    src: {
+        jade: ["src/templates/index.jade"],
+        javascript: ["src/scripts/main.js"]
+    },
+    dest: {
+        jade: "public",
+        javascript: "public/static/scripts/"
+    }
+};
+
+gulp.task("default", ["jade", "scripts"], function taskDefault() {
+    gulp.watch(directories.src.jade, ["jade"]);
+    gulp.watch(directories.src.javascript, ["scripts"]);
 });
 
 // TODO: Task for outputting the template to a single, minified html file
-gulp.task("jade", function taskJade(event) {
-    return gulp.src("./index.jade")
+gulp.task("jade", function taskJade() {
+    return gulp.src(directories.src.jade)
+        .pipe(plumber())
         .pipe(jade({ pretty: true}))
-        .pipe(gulp.dest("./public/"));
+        .pipe(gulp.dest(directories.dest.jade));
+});
+
+// TODO: Compile javascript
+gulp.task("scripts", function taskScripts() {
+    return gulp.src(directories.src.javascript)
+        .pipe(plumber())
+        .pipe(gulp.dest(directories.dest.javascript));
 });
 
 // TODO: Task for outputting scss to minified css
